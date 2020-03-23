9da88d886968ef9f2d4121aa6673855335026140
BevilKunG <devilbevilgamer@gmail.com>
2020-03-23 20:47:28 +0700

    connect mongo db

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+require('dotenv').config()
+
 const app = express()
+
+mongoose.connect(process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PASSWORD))
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
