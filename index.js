5e8dfba7c5bd595598aea733aefa39d169e39a7d
BevilKunG <devilbevilgamer@gmail.com>
2020-03-23 21:21:42 +0700

    setup passport for auth

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const passport = require('passport')
+const LocalStrategy = require('passport-local').Strategy
 const User = require('./models/User')
 require('dotenv').config()
 
@@ -14,11 +16,20 @@ mongoose.connect(
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use(require('express-session')({
+    secret: 'plan-at-home session',
+    resave: false,
+    saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
 
-app.get('/api', (req, res) => {
-    console.log(req.body)
-    res.send('okkk')
-})
+passport.use(new LocalStrategy(User.authenticate()))
+passport.serializeUser(User.serializeUser())
+passport.deserializeUser(User.deserializeUser())
+
+
+
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
