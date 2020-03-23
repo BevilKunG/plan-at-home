339917b132216272103b9400bb0a9e1272655f54
BevilKunG <devilbevilgamer@gmail.com>
2020-03-23 20:59:34 +0700

    config mongoose connection

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const User = require('./models/User')
 require('dotenv').config()
 
 const app = express()
 
-mongoose.connect(process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PASSWORD))
+
+mongoose.connect(
+    process.env.MONGODB_URL.replace('<password>', process.env.MONGODB_PASSWORD), 
+    { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+)
 
 app.use(bodyParser.urlencoded({ extended: true }))
 
@@ -17,5 +22,4 @@ app.get('/api', (req, res) => {
 
 const PORT = process.env.PORT || 5000
 app.listen(PORT, () => {
-    console.log('ok')
 })
