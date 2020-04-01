81920988c598caea4f63ec048fbad631c56f295c
BevilKunG <devilbevilgamer@gmail.com>
2020-04-01 11:47:21 +0700

    add brand margin top

---
+++ client/src/pages/Home/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css'
+
+const Home = () => {
+    return (
+        <div>
+            <div className="brand">
+                <h1 className="display-1 text-center">Plan At Home</h1>
+                <p className="text-center mt-2">Stay productive, work from home</p> 
+            </div>
+        </div>
+    )
+}
+
+export default Home
