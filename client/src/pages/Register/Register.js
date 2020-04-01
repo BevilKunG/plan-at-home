ae35f04d4b674a4b89be8c85fc4caa32f1c1757f
BevilKunG <devilbevilgamer@gmail.com>
2020-04-01 12:05:12 +0700

    change class to className

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,35 +1,38 @@
 import React from 'react'
+import './Register.css'
 
 const Register = () => {
     return (
         <div>
-            <h1 align="center">Register</h1>
+            <h1 className="text-center mt-4">Register</h1>
             <form>
-                    <div class="form-group row">
-                        <label for="staticEmail" class="col-sm-2 col-form-label">Name</label>
-                        <div class="col-sm-10">
-                            <input type="name" class="form-control" id="inputName"></input>
+                    <div className="form-group row my-5">
+                        <label for="inputName" className="col-sm-2 col-form-label">Name</label>
+                        <div className="col-sm-10">
+                            <input type="name" className="form-control" id="inputName"></input>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail"></input>
+                    <div className="form-group row my-5">
+                        <label for="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                        <div className="col-sm-10">
+                            <input type="email" className="form-control" id="inputEmail"></input>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                        <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword"></input>
+                    <div className="form-group row my-5">
+                        <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="inputPassword"></input>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Confirm Password</label>
-                        <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputComFirm"></input>
+                    <div class="form-group row my-5">
+                        <label for="inputComFirm" className="col-sm-2 col-form-label">Confirm Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="inputComFirm"></input>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                    <div className="w-100 text-right">
+                        <button type="submit" className="btn btn-primary mb-2 mr-2">Submit</button>
+                    </div>  
             </form>
         </div>
     )
