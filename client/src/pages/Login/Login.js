46767f985fc4a27dc6fbd365e728b0e0ad141d1f
BevilKunG <devilbevilgamer@gmail.com>
2020-04-01 12:08:07 +0700

    change class to className

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,23 +1,26 @@
 import React from 'react'
+import './Login.css'
 
 const Login = () => {
     return (
         <div>
-            <h1 align="center">Login</h1>
+            <h1 className="text-center mt-4">Login</h1>
                 <form>
-                    <div class="form-group row">
-                        <label for="staticEmail" class="col-sm-2 col-form-label">Email</label>
-                        <div class="col-sm-10">
-                            <input type="email" class="form-control" id="inputEmail"></input>
+                    <div className="form-group row my-5">
+                        <label for="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                        <div className="col-sm-10">
+                            <input type="email" className="form-control" id="inputEmail"></input>
                         </div>
                     </div>
-                    <div class="form-group row">
-                        <label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
-                        <div class="col-sm-10">
-                            <input type="password" class="form-control" id="inputPassword"></input>
+                    <div className="form-group row my-5">
+                        <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                        <div className="col-sm-10">
+                            <input type="password" className="form-control" id="inputPassword"></input>
                         </div>
                     </div>
-                    <button type="submit" class="btn btn-primary mb-2">Submit</button>
+                    <div className="w-100 text-right">
+                        <button type="submit" className="btn btn-primary mb-2 mr-2">Submit</button>
+                    </div>
                 </form>
         </div>
     )
