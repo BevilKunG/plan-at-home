b67200adb118722b298acec4ea7c1f414f48f145
BevilKunG <devilbevilgamer@gmail.com>
2020-04-01 11:53:32 +0700

    increase definition font size

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <div className="brand">
                 <h1 className="display-1 text-center">Plan At Home</h1>
-                <p className="text-center mt-2">Stay productive, work from home</p> 
+                <p className="text-center mt-2 definition-text">Stay productive, work from home</p> 
             </div>
         </div>
     )
