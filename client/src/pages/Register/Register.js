fa9ccd5d067b09fbc5ef0d0cae4c00bcc60d8caf
BevilKunG <devilbevilgamer@gmail.com>
2020-04-03 13:59:38 +0700

    test axios post register

--- client/src/pages/Register/Register.js
+++ client/src/pages/Register/Register.js
@@ -1,41 +1,82 @@
-import React from 'react'
+import React, {Component} from 'react'
+import axios from 'axios'
+import qs from 'qs'
 import './Register.css'
 
-const Register = () => {
-    return (
-        <div>
-            <h1 className="text-center mt-4">Register</h1>
-            <form>
-                    <div className="form-group row my-5">
-                        <label for="inputName" className="col-sm-2 col-form-label">Name</label>
-                        <div className="col-sm-10">
-                            <input type="name" className="form-control" id="inputName"></input>
+class Register extends Component {
+    state = {
+        username: '',
+        email: '',
+        password: '',
+        confirmPassword: ''
+    }
+
+    onFormSubmit = async (e) => {
+        e.preventDefault()
+        const {username, email, password} = this.state
+        const {token} = await axios.post('/api/register', qs.stringify({
+            username,
+            email,
+            password
+        }))
+    }
+
+    render() {
+        return (
+            <div>
+                <h1 className="text-center mt-4">Register</h1>
+                <form onSubmit={this.onFormSubmit}>
+                        <div className="form-group row my-5">
+                            <label htmlFor="inputName" className="col-sm-2 col-form-label">Name</label>
+                            <div className="col-sm-10">
+                                <input 
+                                    type="name" 
+                                    className="form-control" 
+                                    id="inputName"
+                                    onChange={(e) => this.setState({username: e.target.value})}>
+                                </input>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group row my-5">
-                        <label for="inputEmail" className="col-sm-2 col-form-label">Email</label>
-                        <div className="col-sm-10">
-                            <input type="email" className="form-control" id="inputEmail"></input>
+                        <div className="form-group row my-5">
+                            <label htmlFor="inputEmail" className="col-sm-2 col-form-label">Email</label>
+                            <div className="col-sm-10">
+                                <input 
+                                    type="email" 
+                                    className="form-control" 
+                                    id="inputEmail"
+                                    onChange={(e) => this.setState({email: e.target.value})}>
+                                </input>
+                            </div>
                         </div>
-                    </div>
-                    <div className="form-group row my-5">
-                        <label for="inputPassword" className="col-sm-2 col-form-label">Password</label>
-                        <div className="col-sm-10">
-                            <input type="password" className="form-control" id="inputPassword"></input>
+                        <div className="form-group row my-5">
+                            <label htmlFor="inputPassword" className="col-sm-2 col-form-label">Password</label>
+                            <div className="col-sm-10">
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="inputPassword"
+                                    onChange={(e) => this.setState({password: e.target.value})}>
+                                </input>
+                            </div>
                         </div>
-                    </div>
-                    <div class="form-group row my-5">
-                        <label for="inputComFirm" className="col-sm-2 col-form-label">Confirm Password</label>
-                        <div className="col-sm-10">
-                            <input type="password" className="form-control" id="inputComFirm"></input>
+                        <div className="form-group row my-5">
+                            <label htmlFor="inputComFirm" className="col-sm-2 col-form-label">Confirm Password</label>
+                            <div className="col-sm-10">
+                                <input 
+                                    type="password" 
+                                    className="form-control" 
+                                    id="inputComFirm"
+                                    onChange={(e) => this.setState({confirmPassword: e.target.value})}>
+                                </input>
+                            </div>
                         </div>
-                    </div>
-                    <div className="w-100 text-right">
-                        <button type="submit" className="btn btn-primary mb-2 mr-2">Submit</button>
-                    </div>  
-            </form>
-        </div>
-    )
+                        <div className="w-100 text-right">
+                            <button type="submit" className="btn btn-primary mb-2 mr-2">Submit</button>
+                        </div>  
+                </form>
+            </div>
+        )
+    }
 }
 
 export default Register
--- routes/api/register.js
+++ routes/api/register.js
@@ -9,11 +9,11 @@ const User = require('../../models/User')
 router.post(
     '/', 
     [
-        check('username', 'Please enter a valid username')
-        .not()
-        .isEmpty(),
-        check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a valid password').isLength({ min: 6 })
+        // check('username', 'Please enter a valid username')
+        // .not()
+        // .isEmpty(),
+        // check('email', 'Please enter a valid email').isEmail(),
+        // check('password', 'Please enter a valid password').isLength({ min: 6 })
     ],
     async (req, res) => {
         const errors = validationResult(req)
