f49f8229f192f7a1c471c16caf34185bbe003d51
BevilKunG <devilbevilgamer@gmail.com>
2020-04-03 15:20:58 +0700

    fetch user with token

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {BrowserRouter,Switch ,Route} from 'react-router-dom'
 import Layout from './components/Layout'
 import {Home, Register, Login} from './pages'
+import axios from 'axios'
 
-const App = () => {
-    return (
-        <BrowserRouter>
-            <Layout>
-                <Switch>
-                    <Route exact path="/" component={Home}/>
-                    <Route path="/register" component={Register}/>
-                    <Route path="/login" component={Login}/>
-                </Switch>
-            </Layout>
-        </BrowserRouter>
-    )
+class App extends Component {
+    state = {
+        token: null,
+        user: null
+    }
+
+    componentDidMount() {
+        if(!this.state.user && this.state.token !== null) {
+            this.fetchUser(this.state.token)
+        }
+    }
+
+    setToken = (token) => {
+        console.log(token)
+        this.setState({ token })
+        this.fetchUser(token)
+    }
+
+    fetchUser = (token) => {
+        axios.get('/api/user', {
+            headers: {
+                token
+            }
+        }).then((res) => {
+            const {user} = res.data
+            this.setState({ user })
+        })
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+                <Layout>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="/register" component={() => <Register setToken={this.setToken} />}/>
+                        <Route path="/login" component={Login}/>
+                    </Switch>
+                </Layout>
+            </BrowserRouter>
+        )
+    }
 }
 
 export default App
