66099d23d6a14e3d0b2ebdbdf8c4cd77dba974f3
BevilKunG <devilbevilgamer@gmail.com>
2020-04-05 09:43:51 +0700

    render auth button

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,51 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Link} from 'react-router-dom'
+import {connect} from 'react-redux'
+import {setToken, setUser} from '../actions'
 
-const Navbar = () => {
-    return (
-        <nav className="navbar navbar-dark bg-primary">
-            <Link 
-                className="navbar-brand mb-0 h1" 
-                to="/">Plan At Home</Link>
+class Navbar extends Component {
+    logout = () => {
+        this.props.setToken(null)
+        this.props.setUser(null)
+    }
+
+    renderAuthButton() {
+        if(!this.props.user) {
+            return (
+                <div>
+                    <Link 
+                        className="btn btn-outline-light mr-2" 
+                        to="/register">Register</Link>
+                    <Link 
+                        className="btn btn-outline-light mr-2" 
+                        to="/login">Login</Link>
+                </div>
+            )
+        }
+
+        return (
             <div>
-                <Link 
-                    className="btn btn-outline-light mr-2" 
-                    to="/register">Register</Link>
-                <Link 
-                    className="btn btn-outline-light mr-2" 
-                    to="/login">Login</Link>
+                    <button 
+                        className="btn btn-outline-light mr-2" 
+                        onClick={this.logout}>Logout</button>
             </div>
-        </nav>
-    )
+        )
+    }
+
+    render() {
+        return (
+            <nav className="navbar navbar-dark bg-primary">
+                <Link 
+                    className="navbar-brand mb-0 h1" 
+                    to="/">Plan At Home</Link>
+                {this.renderAuthButton()}
+            </nav>
+        )
+    }
 }
 
-export default Navbar
+const mapStateToProps = (state) => {
+    return state
+}
+
+export default connect(mapStateToProps, {setToken, setUser})(Navbar)
