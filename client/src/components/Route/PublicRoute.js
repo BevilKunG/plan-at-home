b531b97b157ee8ea0fb763eb926ec425dc407154
BevilKunG <devilbevilgamer@gmail.com>
2020-04-05 09:30:30 +0700

    check user from store

--- client/src/components/Route/PublicRoute.js
+++ client/src/components/Route/PublicRoute.js
@@ -1,15 +1,20 @@
 
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import {connect} from 'react-redux'
 
 const PublicRoute = ({component: Component, restricted, user, ...rest}) => {
     return (
         <Route {...rest} render={props => (
             user !== null && restricted ?
-                <Redirect to="/dashboard" />
+                <Redirect to="/plan" />
             : <Component {...props} />
         )} />
     )
 }
 
-export default PublicRoute
+const mapStateToProps = (state) => {
+    return state
+}
+
+export default connect(mapStateToProps)(PublicRoute)
