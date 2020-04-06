1bbc915a14bf73da094b73367b9d35b45cb2f51e
BevilKunG <devilbevilgamer@gmail.com>
2020-04-06 18:59:03 +0700

    add plan routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,4 +5,6 @@ router.use('/register', require('./register'))
 router.use('/login', require('./login'))
 router.use('/user', require('./user'))
 
+router.use('/plans', require('./plan'))
+
 module.exports = router
---
+++ routes/api/plan.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const router = express.Router()
+const Plan = require('../../models/Plan')
+
+router.get('/', (req, res) => {
+    Plan.find({}, (error, plans) => {
+        if(error) {
+            res.status(500).json(error)
+        }
+        res.status(200).json(plans)
+    })
+})
+
+router.post('/', (req, res) => {
+    const {name, date, activities} = req.body
+    Plan.create({
+        name,
+        date,
+        activities
+    }, (error, plan) => {
+        if(error) {
+            res.status(500).json(error)
+        }
+        res.status(201).json(plan)
+    })
+})
+
+router.post('/push', (req, res) => {
+    const plans = [
+        {
+            name: 'Today Plan',
+            date: Date.now(),
+            activities: [
+                { name: 'read a book', duration: '06.00-07.00' },
+                { name: 'do exercise', duration: '08.00-09.00' }
+            ]
+        },
+        {
+            name: 'Tomorrow Plan',
+            date: new Date(2020, 4, 6),
+            activities: [
+                { name: 'read a book', duration: '06.00-07.00' },
+                { name: 'do exercise', duration: '08.00-09.00' }
+            ]
+        },
+    ]
+    
+    Plan.insertMany(plans, (error, doc) => {
+        if(error) {
+            res.status(500).json(error)
+        }
+        res.status(201).send('push success')
+    })
+})
+
+module.exports = router
