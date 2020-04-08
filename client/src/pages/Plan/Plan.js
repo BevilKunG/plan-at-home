85a9419392b38740f4eacc90b4ec187dd71a9a3d
BevilKunG <devilbevilgamer@gmail.com>
2020-04-08 07:52:43 +0700

    fetch plans in page for dev

--- client/src/pages/Plan/Plan.js
+++ client/src/pages/Plan/Plan.js
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {PlanList} from '../../components/Plan'
+import {fetchPlans} from '../../actions/index'
 
-const Plan = (props) => {
-    return (
-        <div>
-            <PlanList />
-        </div>
-    )
+class Plan extends Component {
+    componentDidMount() {
+        this.props.fetchPlans()
+    }
+
+    render() {
+        return (
+            <div>
+                <PlanList />
+            </div>
+        )
+    }
 }
 
 const mapStateToProps = (state) => {
     return state
 }
 
-export default connect(mapStateToProps)(Plan)
+export default connect(mapStateToProps, {fetchPlans})(Plan)
