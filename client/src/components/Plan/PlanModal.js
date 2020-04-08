9671046f4e5a409bd52d6fb4279b442259d352a8
BevilKunG <devilbevilgamer@gmail.com>
2020-04-08 09:38:14 +0700

    handle plan state

--- client/src/components/Plan/PlanModal.js
+++ client/src/components/Plan/PlanModal.js
@@ -2,23 +2,82 @@ import React, {Component} from 'react'
 import {Modal, Button} from 'react-bootstrap'
 
 class PlanModal extends Component {
+    state = {
+        name: '',
+        d: '',
+        m: '',
+        y: ''
+    }
+
+    clearState = () => {
+        this.setState({
+            name: '',
+            d: '',
+            m: '',
+            y: ''
+        })
+    }
+
+    onHide = () => {
+        this.clearState()
+        this.props.hideModal()
+    }
+
     render() {
         return (
             <Modal 
                 show={this.props.show}
-                onHide={this.props.hideModal}>
+                onHide={this.onHide}>
                 <Modal.Header closeButton>
                     <Modal.Title>New Plan</Modal.Title>
                 </Modal.Header>
 
                 <Modal.Body>
                     <form>
-                        <div className="form-group">
-                            <label>Plan Name</label>
-                            <input
-                                type="text"
-                                className="form-control"
-                                placeholder="Plan Name" />
+                        <div className="form-group row">
+                            <label className="col-2 col-form-label">Name</label>
+                            <div className="col-10">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Name"
+                                    value={this.state.name}
+                                    onChange={(e) => this.setState({ name: e.target.value })} />
+                            </div>
+                        </div>
+
+                        <div className="form-group row">
+                            <label className="col-2 col-form-label">Date</label>
+                            <div className="col-10">
+                                <div className="row">
+                                    <div className="col">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="DD"
+                                            value={this.state.d}
+                                            onChange={(e) => this.setState({ d: e.target.value })} />
+                                    </div>
+
+                                    <div className="col">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="MM"
+                                            value={this.state.m}
+                                            onChange={(e) => this.setState({ m: e.target.value })} />
+                                    </div>
+
+                                    <div className="col">
+                                        <input
+                                            type="text"
+                                            className="form-control"
+                                            placeholder="YYYY"
+                                            value={this.state.y}
+                                            onChange={(e) => this.setState({ y: e.target.value })} />
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </form>
                 </Modal.Body>
@@ -26,7 +85,7 @@ class PlanModal extends Component {
                 <Modal.Footer>
                     <button 
                         className="btn btn-secondary"
-                        onClick={this.props.hideModal}>Close</button>
+                        onClick={this.onHide}>Close</button>
                     <button className="btn btn-primary">Add</button>
                 </Modal.Footer>
             </Modal>
