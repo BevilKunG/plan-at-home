ecd1f28e50fc0163863b99602fde502aedc37c1f
BevilKunG <devilbevilgamer@gmail.com>
2020-04-08 20:57:26 +0700

    fix rename plan reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,10 @@
 import {combineReducers} from 'redux'
 import userReducer from './userReducer'
 import tokenReducer from './tokenReducer'
-import plansReducer from './plansReducer'
+import planReducer from './planReducer'
 
 export default combineReducers({
     user: userReducer,
     token: tokenReducer,
-    plans: plansReducer
+    plans: planReducer
 })
