4518b6c58709e7896ef4bbb849d176dd2e20462e
BevilKunG <devilbevilgamer@gmail.com>
2020-04-09 23:14:00 +0700

    add home background in public

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,7 +4,12 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-            <div className="home-background"></div>
+            <div 
+                className="home-background"
+                style={{
+                    backgroundImage: `url(${process.env.PUBLIC_URL}/home-background.jpg)`
+                }}>    
+            </div>
             <div className="brand-box">
                 <h1 className="display-1 text-center">Plan At Home</h1>
                 <p className="text-center mt-2 definition-text">Stay productive, work from home</p> 
