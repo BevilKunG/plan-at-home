bc51ff0f6b817179365c0625006e623107a8c1c1
BevilKunG <devilbevilgamer@gmail.com>
2020-04-09 13:20:07 +0700

    validate plan

--- client/src/components/Plan/Plan/PlanModal.js
+++ client/src/components/Plan/Plan/PlanModal.js
@@ -10,15 +10,31 @@ class PlanModal extends Component {
         name: '',
         d: '',
         m: '',
-        y: ''
+        y: '',
+        errorMessage: '',
+        nameError: false,
+        dError: false,
+        mError: false,
+        yError: false
     }
 
     clearState = () => {
+        this.clearError()
         this.setState({
             name: '',
             d: '',
             m: '',
             y: ''
+        })
+    }
+
+    clearError = () => {
+        this.setState({
+            errorMessage: '',
+            nameError: false,
+            dError: false,
+            mError: false,
+            yError: false
         })
     }
 
@@ -28,6 +44,7 @@ class PlanModal extends Component {
         const d = `${date.getDate()}`
         const m = `${date.getMonth()+1}`
         const y = `${date.getFullYear()}`
+        this.clearError()
         this.setState({
             name,
             d,
@@ -43,8 +60,49 @@ class PlanModal extends Component {
         this.props.hideModal()
     }
 
+    validatePlan = () => {
+        this.clearError()
+        
+        const {name, d, m, y} = this.state
+        if(!/[a-z A-Z 0-9]+/g.test(name)) {
+            this.setState({ 
+                errorMessage: 'Invalid name',
+                nameError: true
+            })
+            return false
+        }
+
+        if(!/^[0-9]{2}$/g.test(d) || parseInt(d) < 1 || parseInt(d)>31) {
+            this.setState({ errorMessage: 
+                'Date must be 01-31 (DD)',
+                dError: true
+            })
+            return false
+        }
+
+        if(!/^[0-9]{2}$/g.test(m) || parseInt(m) < 1 || parseInt(m)>12) {
+            this.setState({ 
+                errorMessage: 'Month must be 01-12 (MM)',
+                mError: true
+            })
+            return false
+        }
+
+        if(!/^[0-9]+$/g.test(y)) {
+            this.setState({ 
+                errorMessage: 'Year must be contain digits (YYYY)',
+                yError: true
+            })
+            return false
+        }
+
+        return true
+    }
+
 
     addPlan = () => {
+        if(!this.validatePlan()) return
+        
         const {name, d, m, y} = this.state
         const date = new Date(`${y}-${m}-${d}`)
         const activities = []
@@ -61,6 +119,8 @@ class PlanModal extends Component {
     }
 
     updatePlan = () => {
+        if(!this.validatePlan()) return
+
         const {name, d, m, y} = this.state
         const date = new Date(`${y}-${m}-${d}`)
         const {activities} = this.props.plan
@@ -99,6 +159,15 @@ class PlanModal extends Component {
         
     }
 
+    renderAlert() {
+        if(! this.state.errorMessage) return null
+        return (
+            <div className="alert alert-danger">
+                {this.state.errorMessage}
+            </div>
+        )
+    }
+
     render() {
         return (
             <Modal 
@@ -109,13 +178,14 @@ class PlanModal extends Component {
                 </Modal.Header>
 
                 <Modal.Body>
+                    {this.renderAlert()}
                     <form>
                         <div className="form-group row">
                             <label className="col-2 col-form-label">Name</label>
                             <div className="col-10">
                                 <input
                                     type="text"
-                                    className="form-control"
+                                    className={`form-control ${this.state.nameError?'is-invalid':''}`}
                                     placeholder="Name"
                                     value={this.state.name}
                                     onChange={(e) => this.setState({ name: e.target.value })} />
@@ -129,7 +199,7 @@ class PlanModal extends Component {
                                     <div className="col">
                                         <input
                                             type="text"
-                                            className="form-control"
+                                            className={`form-control ${this.state.dError?'is-invalid':''}`}
                                             placeholder="DD"
                                             value={this.state.d}
                                             onChange={(e) => this.setState({ d: e.target.value })} />
@@ -138,7 +208,7 @@ class PlanModal extends Component {
                                     <div className="col">
                                         <input
                                             type="text"
-                                            className="form-control"
+                                            className={`form-control ${this.state.mError?'is-invalid':''}`}
                                             placeholder="MM"
                                             value={this.state.m}
                                             onChange={(e) => this.setState({ m: e.target.value })} />
@@ -147,7 +217,7 @@ class PlanModal extends Component {
                                     <div className="col">
                                         <input
                                             type="text"
-                                            className="form-control"
+                                            className={`form-control ${this.state.yError?'is-invalid':''}`}
                                             placeholder="YYYY"
                                             value={this.state.y}
                                             onChange={(e) => this.setState({ y: e.target.value })} />
