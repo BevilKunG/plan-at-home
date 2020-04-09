5b07b7749f0ac4660be421251e153d66ce14f5b7
BevilKunG <devilbevilgamer@gmail.com>
2020-04-09 19:54:49 +0700

    add footer

--- client/src/components/Layout.js
+++ client/src/components/Layout.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import Navbar from './Navbar'
+import Footer from './Footer'
+import './Layout.css'
 
 const Layout = (props) => {
     return (
         <div>
             <Navbar />
-            <div className="container">
+            <div className="container fullpage">
                 {props.children}
             </div>
+            <Footer/>
         </div>
     )
 }
