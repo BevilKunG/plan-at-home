f0e7ee4aa43514eea23fe4f3041936ac7cb0f289
BevilKunG <devilbevilgamer@gmail.com>
2020-04-09 08:53:41 +0700

    add home background

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,7 +4,8 @@ import './Home.css'
 const Home = () => {
     return (
         <div>
-            <div className="brand">
+            <div className="home-background"></div>
+            <div className="brand-box">
                 <h1 className="display-1 text-center">Plan At Home</h1>
                 <p className="text-center mt-2 definition-text">Stay productive, work from home</p> 
             </div>
